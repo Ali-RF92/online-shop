5b547aaaf6ddd62237215fc8641f9638cf323050
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-02-09 19:28:21 +0100

    create blueprints(admin, user) update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from blueprints.general import app as general_app
-
+from blueprints.admin import app as admin_app
+from blueprints.user import app as user_app
 
 app = Flask(__name__)
 app.register_blueprint(general_app)
+app.register_blueprint(admin_app)
+app.register_blueprint(user_app)
 
 
 
